70cc94da2fe114634095b30fab715df15dc5a69b
blackninja81 <johnkaburu81@gmail.com>
2023-01-04 16:23:23 +0300

    finished up on store frontend

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,16 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import '../styles/Styles.scss'
+// Default theme
+import '@splidejs/react-splide/css';
+
+
+// or other themes
+import '@splidejs/react-splide/css/skyblue';
+import '@splidejs/react-splide/css/sea-green';
+
+
+// or only core styles
+import '@splidejs/react-splide/css/core';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
